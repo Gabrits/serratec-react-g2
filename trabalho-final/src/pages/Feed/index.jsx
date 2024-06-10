ca7d1c0fe4c48fab8ffbe2d08d3f8fdf0b12b321
muryarth <arthur.mury@outlook.com>
2024-06-10 04:54:18 -0300

    feat: feed retorna informações para o usuário autenticado

--- trabalho-final/src/pages/Feed/index.jsx
+++ trabalho-final/src/pages/Feed/index.jsx
@@ -1,7 +1,49 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
+import { getCookie } from "../../services/cookie";
+import AppButton from "../../components/AppButton";
 
 function Feed() {
-  return <div>Feed</div>;
+  const [publicacoes, setPublicacoes] = useState([]);
+
+  const getAllPosts = () => {
+    const jwtToken = getCookie("jwtToken");
+
+    if (jwtToken) {
+      axios
+        .get("http://localhost:9090/publicacoes", {
+          headers: {
+            Authorization: jwtToken,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          setPublicacoes(response.data);
+        })
+        .catch((error) => error.message);
+    }
+  };
+
+  useEffect(getAllPosts, []);
+
+  return (
+    <>
+      {publicacoes.length > 0 ? (
+        <ul>
+          {publicacoes.map((publicacao, index) => (
+            <li key={index}>
+              <p>Autor: {publicacao.autor}</p>
+              <p>Conteúdo: {publicacao.conteudo}</p>
+              <p>Data da publicação: {publicacao.dataCriacao}</p>
+              <br />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        "Não há dados para serem exibidos no Feed!"
+      )}
+    </>
+  );
 }
 
 export default Feed;
