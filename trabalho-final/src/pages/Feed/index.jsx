60c1e3193de467cbfbde2601a7cbc10770fc25b1
Gabrits <162373342+Gabrits@users.noreply.github.com>
2024-06-11 17:48:32 -0300

    feat: finalizando feed

--- trabalho-final/src/pages/Feed/index.jsx
+++ trabalho-final/src/pages/Feed/index.jsx
@@ -2,12 +2,20 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { getCookie } from "../../services/cookie";
 import AppButton from "../../components/AppButton";
-import "./style.css";
+import './style.css'
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { HiHeart } from "react-icons/hi";
-import Navbar from "../../components/Navbar";
+import { FaPencilAlt } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import Button from 'react-bootstrap/Button';
+import { sendDeleteRequest } from "../../services/requests";
 
 function Feed() {
+  const [curtido, setCurtido] = useState(false);
+
+  const handleClickCurtir = () => {
+    setCurtido(!curtido);
+  }
   const [publicacoes, setPublicacoes] = useState([]);
 
   const getAllPosts = () => {
@@ -28,58 +36,51 @@ function Feed() {
     }
   };
 
-  useEffect(getAllPosts, []);
+  const handleDelete = ( id ) => {
+    sendDeleteRequest(id,"publicacoes")
+    .then(() => setPublicacoes(publicacoes.filter(publicacao => publicacao.id !== id)))
+  }
 
+
+  useEffect(getAllPosts, []);
+  
   return (
     <>
-      <Navbar />
+    <div className="novaPostagem">
+      <Button variant="primary">Adicionar Postagem</Button>
+    </div>
+      
       {publicacoes.length > 0 ? (
-        <ul>
+        <ul className="containerGeral">
           {publicacoes.map((publicacao, index) => (
             <li key={index}>
               <div className="container">
                 <div className="publicacao">
                   <div className="nomefoto">
                     <div className="fotoPerfil">
-                      <img
-                        src="https://i.pinimg.com/474x/fa/ba/54/faba5498b3167071dc93e22f3ce1e22a.jpg"
-                        alt=""
-                        width={50}
-                        height={50}
-                      />
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP3lC0SfgqCcTGipFh64hddM6xgBYQj90wOA&s" alt="" width={50} height={50}/>
                     </div>
-
                     <div className="nomePerfil">
                       <p>{publicacao.autor}</p>
                     </div>
                   </div>
-
-                  <div className="imagem">
-                    <img src={publicacao.conteudo} width={300} height={300} />
+                  <div className="imagem">  
+                    <img src={publicacao.conteudo} width={300} height={300}/>
                   </div>
-
-                  <div className="curtirComentarEditar">
-                    <div className="curtir">
-                      <HiHeart />
-                    </div>
-
-                    {/* <div className="comentar">
-              <img src="src/assets/comentarioBranco.png" width={30} height={39} />
-            </div> */}
-
-                    <div className="editar">
+                  <ul className="emojis">
+                    <li className={curtido ? "curtir ativo" : "curtir"} onClick={handleClickCurtir}>
+                      <FaHeart />
+                    </li>
+                    <li className="editar">
                       <Link to={`/feedMore/${publicacao.id}`}>
-                        <img
-                          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/White_pencil_icon.svg/800px-White_pencil_icon.svg.png"
-                          width={20}
-                          height={40}
-                        />
+                        <FaPencilAlt />
                       </Link>
-                    </div>
-                  </div>
-                  {/* <div className="descricao">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, minus fugiat? Quos quo quibusdam, laudantium iure ducimus consequuntur quidem culpa sint, odio error fugit nulla architecto, atque esse aut maxime.</p>
-          </div> */}
+                    </li>
+                    <li onClick = {()=> handleDelete(publicacao.id)} className="deletar">
+                      
+                      <FaTrash />
+                    </li>
+                  </ul>
                 </div>
               </div>
             </li>
